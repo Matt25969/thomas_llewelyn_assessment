749416f760e13ab3ead688c099d3731a1ed7c053
TomLLew <thomas.llewelyn@academytrainee.com>
2019-12-04 21:08:54 +0000

    all routes added, basic html

--- application/routes.py
+++ application/routes.py
@@ -2,10 +2,22 @@ from flask import render_template
 from application import app
 
 @app.route('/')
-@app.route('/home')
-def home ():
-    return render_template('home.html', title='Home')
-
 @app.route('/about')
-def about():
+def home ():
     return render_template('about.html', title='About')
+
+@app.route('/log')
+def about():
+    return render_template('log.html', title='Log')
+
+@app.route('/account')
+def account():
+    return render_template('account.html', title='Account')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Login')
+
+@app.route('/register')
+def register():
+    return render_template('register.html', title='Register')
