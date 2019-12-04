2e12aa4297e211e59afbcaceb6e0d689e71ff280
TomLLew <thomas.llewelyn@academytrainee.com>
2019-12-04 23:46:26 +0000

    got a connection to DB

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' +os.getenv('MYSQL_USER')+ ':' +os.getenv('MYSQL_PASS')+ '@' +os.getenv('MYSQL_IP')+ '/llewthenics'
+db = SQLAlchemy(app)
 
 from application import routes
---
+++ application/models.py
@@ -0,0 +1,38 @@
+from application import db
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+class log(db.Model):
+	id = db.Column(db.Integer, primary_key=True, nullable=False)
+	user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False) 
+	date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+	workout = db.Column(db.String(100), nullable=False)
+	sets = db.Column(db.Integer, nullable=False)
+	reps = db.Column(db.Integer, nullable=False)
+	body_part = db.Column(db.String(100), nullable=False)
+
+	def __repr__(self):
+		return ''.join([
+			'Date Posted: ', self.date_posted, '\r\n',
+			'Workout: ', self.body_part,' - ', self.workout, '\r\n',
+			'Sets: ', self.sets, '\r\n',
+			'Reps: ', self.reps, '\r\n',
+			'User: ', self.user_id
+		])
+			
+			
+	
+class users(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	first_name = db.Column(db.String(60), nullable=False)
+	last_name = db.Column(db.String(60), nullable=False)
+	email = db.Column(db.String(150), nullable=False, unique=True)
+	password = db.Column(db.String(50), nullable=False)
+	log = db.relationship('Workout_Log', backref='log', lazy=True)
+
+	def __repr__(self):
+		return ''.join([
+			'User ID: ', str(self.id), '\r\n',
+			'Name: ', self.first_name, ' ', self.lastname, '\r\n',
+			'Email: ', self.email
+		])
